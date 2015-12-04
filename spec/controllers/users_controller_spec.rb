2980cd5da4fb19af582a0a6dce9fab40415eb2f5
Alexandre Monjol <alexandre.monjol@gmail.com>
2015-12-04 12:46:50 +0100

    remove f test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,20 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  render_views
-
-  describe "GET home" do
-    it "returns http success and a homepage" do
-      get :home
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET home" do
-    it "returns http success and a homepage" do
-      get :home
-      expect(response).to have_http_status(:success)
-    end
-  end
+  
 
 end
