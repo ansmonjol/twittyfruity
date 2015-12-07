2f56e217debf5849a44bdea016d1bfd847f9bfef
Alexandre Monjol <alexandre.monjol@gmail.com>
2015-12-07 22:16:44 +0100

    admin route security

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  authenticated :user, lambda {|u| u.admin } do
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  end
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'home#index'
 
   resources :users do
     post :follow
@@ -14,6 +16,8 @@ Rails.application.routes.draw do
     resources :tweets
   end
 
+  root 'home#index'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
