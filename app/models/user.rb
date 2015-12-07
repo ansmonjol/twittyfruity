e64ace2679cc0ad90388171a59d9986e50fa057e
Alexandre Monjol <alexandre.monjol@gmail.com>
2015-12-07 13:01:41 +0100

    add devise auth with email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,38 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
 
   # Associations
   has_many :tweets
   has_many :followers, :class_name => 'Follower', :foreign_key => 'user_id'
   has_many :following, :class_name => 'Follower', :foreign_key => 'follower_id'
+
+  # Validations
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  # Set username to user authentification
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
 
   # Prety user display for rails_admin
   def to_s
